1fbd43c5424ecc9d66f5316eb370d33d6c5022ae
DilaTeknikInformatika <90737225+DilaTeknikInformatika@users.noreply.github.com>
2022-01-19 23:16:30 +0700

    Android Activity and Intent

---
+++ Tugas3/app/src/main/java/com/dila/myinstangrm/DetailActivity.kt
@@ -0,0 +1,20 @@
+package com.dila.myinstangrm
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+
+        val btn_back = findViewById<Button>(R.id.btn_back)
+
+        btn_back.setOnClickListener {
+            val back = Intent(this@DetailActivity, MainActivity::class.java)
+            startActivity(back)
+        }
+    }
+}
